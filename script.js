1ae4d4be298a70cd7a897f38e64ca02bb573b9cf
huzitaiwo <taiwohusayn5@gmail.com>
2021-10-27 22:06:23 +0100

    created script.js file and added some coool animation to the cards

---
+++ script.js
@@ -0,0 +1,16 @@
+const designGrids = document.querySelectorAll('.design-image-grid > div');
+const cards = document.querySelectorAll('.card');
+const brands = document.querySelectorAll('.card__brand');
+const cardButtons = document.querySelectorAll('.card__button');
+
+cards.forEach((card,i) => {
+    card.addEventListener('mouseenter', () => {
+        card.classList.add('active');
+        brands[i].style.transform = 'translateY(-30px)';
+        cardButtons[i].classList.add('active');
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.classList.remove('active');
+    });
+});
