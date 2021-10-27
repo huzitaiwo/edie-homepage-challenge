97d72b8ecf4aeded55ce13ae8400174bcdbfc39d
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-10-27 22:55:58 +0100

    fixed the mobile reponsiveness bug

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ const cards = document.querySelectorAll('.card');
 const brands = document.querySelectorAll('.card__brand');
 const cardButtons = document.querySelectorAll('.card__button');
 
-cards.forEach((card,i) => {
-    card.addEventListener('mouseenter', () => {
-        card.classList.add('active');
-        brands[i].style.transform = 'translateY(-20px)';
-        cardButtons[i].classList.add('active');
-    });
 
-    card.addEventListener('mouseleave', () => {
-        card.classList.remove('active');
-        brands[i].style.transform = 'translateY(0)';
-        cardButtons[i].classList.remove('active');
+const desktop = window.matchMedia("(min-width: 1000px)")
+
+if(desktop.matches) {
+
+    cards.forEach((card,i) => {
+        card.addEventListener('mouseenter', () => {
+            card.classList.add('active');
+            brands[i].style.transform = 'translateY(-20px)';
+            cardButtons[i].classList.add('active');
+        });
+
+        card.addEventListener('mouseleave', () => {
+            card.classList.remove('active');
+            brands[i].style.transform = 'translateY(0)';
+            cardButtons[i].classList.remove('active');
+        });
     });
-});
+    
+};
