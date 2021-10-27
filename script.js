12436afa00f2180937a9847b4ad61ee8f2a8d74e
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-10-27 22:51:06 +0100

    implemented mobile reponsiveness

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const cardButtons = document.querySelectorAll('.card__button');
 cards.forEach((card,i) => {
     card.addEventListener('mouseenter', () => {
         card.classList.add('active');
-        brands[i].style.transform = 'translateY(-30px)';
+        brands[i].style.transform = 'translateY(-20px)';
         cardButtons[i].classList.add('active');
     });
 
     card.addEventListener('mouseleave', () => {
         card.classList.remove('active');
+        brands[i].style.transform = 'translateY(0)';
+        cardButtons[i].classList.remove('active');
     });
 });
